9572a57a74bc124013ac170997ce1b3129dfa404
CalacasChidas <ale-jimenez@estudiantec.cr>
2023-10-26 23:10:26 -0600

    Interfáz
    Casi terminada

--- main.cpp
+++ main.cpp
@@ -7,32 +7,50 @@
 #include <cstdlib>
 
 GtkWidget *window;
-GtkWidget *botones[10][10], *botonPrueba;
-GtkLabel *cantKoban;
-int clientSocket;
-int cantK = 1 + rand() % 100;
+GtkWidget *botones[10][10], *iter, *yr, *af, *tn;
+GtkComboBoxText *obstaculos;
+GtkLabel *cantKoban, *advertencias;
+int clientSocket, obstype;
+int cantK = 9;
 char buffer[10];
 
-
 void on_button_clicked(GtkWidget *widget, gpointer data) {
-    std::cout<<"Funciona\n";
-    const gchar *nombre = gtk_widget_get_name(widget);
-    if (nombre != nullptr) {
-        // Imprimir el nombre del botón
-        std::cout << "Nombre del botón: " << nombre << std::endl;
+    switch (obstype) {
+        // Verificamos si el widget es un botón
+        case 1:
+            gtk_button_set_label(GTK_BUTTON(widget), "1");
+            obstype = 0;
+            gtk_label_set_text(advertencias, " ");
+            break;
+        case 2:
+            gtk_button_set_label(GTK_BUTTON(widget), "2");
+            obstype = 0;
+            gtk_label_set_text(advertencias, " ");
+            break;
+        case 3:
+            gtk_button_set_label(GTK_BUTTON(widget), "3");
+            obstype = 0;
+            gtk_label_set_text(advertencias, " ");
+            break;
+        default:
+            gtk_label_set_text(advertencias, "Seleccione un obstáculo primero!");
+            std::cout << "Obstáculo no seleccionado\n";
+            break;
     }
-    /*
-    const gchar *nombre = gtk_widget_get_name(widget);
-    if (nombre != nullptr && strncmp(nombre, "b", 1) == 0) {
-        // Extraer la fila y la columna del nombre del botón
-        int fila, columna;
-        if (sscanf(nombre + 1, "%d%d", &fila, &columna) == 2) {
-            // Imprimir las coordenadas
-            std::cout << "Fila: " << fila << ", Columna: " << columna << std::endl;
-        }
-    }
-     */
 }
+void yari(GtkWidget *widget, gpointer data) {
+    obstype = 1;
+    std::cout<< "Obstáculo seleccionado: Yari\n";
+}
+void ayf(GtkWidget *widget, gpointer data) {
+    obstype = 2;
+    std::cout<< "Obstáculo seleccionado: Arco y flecha\n";
+}
+void tna(GtkWidget *widget, gpointer data) {
+    obstype = 3;
+    std::cout<< "Obstáculo seleccionado: Tanegashima\n";
+}
+
 int main(int argc, char *argv[]) {
     sprintf(buffer, "%d", cantK);
     GtkBuilder *builder;
@@ -42,26 +60,38 @@ int main(int argc, char *argv[]) {
 
     //Declaración de objetos
     window = GTK_WIDGET(gtk_builder_get_object(builder, "Window"));
-    GtkWidget *grid = gtk_grid_new();
+
 
     for (int fila = 0; fila < 10; fila++) {
         for (int columna = 0; columna < 10; columna++) {
             char nombreBoton[20];
             sprintf(nombreBoton, "b%d%d", fila, columna);
             botones[fila][columna] = GTK_WIDGET(gtk_builder_get_object(builder, nombreBoton));
-            std::cout<<nombreBoton;
+            //std::cout<<nombreBoton;
 
             g_signal_connect(G_OBJECT(botones[fila][columna]), "clicked", G_CALLBACK(on_button_clicked), NULL);
         }
     }
-    botonPrueba = GTK_WIDGET(gtk_builder_get_object(builder, "BotonPrueba"));
-    g_signal_connect(G_OBJECT(botonPrueba), "clicked", G_CALLBACK(on_button_clicked), NULL);
+
+    iter = GTK_WIDGET(gtk_builder_get_object(builder, "iteracion"));
+    g_signal_connect(G_OBJECT(iter), "clicked", G_CALLBACK(on_button_clicked), NULL);
+
+    yr = GTK_WIDGET(gtk_builder_get_object(builder, "yr"));
+    g_signal_connect(G_OBJECT(yr), "clicked", G_CALLBACK(yari), NULL);
+
+    af = GTK_WIDGET(gtk_builder_get_object(builder, "af"));
+    g_signal_connect(G_OBJECT(af), "clicked", G_CALLBACK(ayf), NULL);
+
+    tn = GTK_WIDGET(gtk_builder_get_object(builder, "tn"));
+    g_signal_connect(G_OBJECT(tn), "clicked", G_CALLBACK(tna), NULL);
+
 
     cantKoban = GTK_LABEL(gtk_builder_get_object(builder, "cantKoban"));
+    advertencias = GTK_LABEL(gtk_builder_get_object(builder, "advertencias"));
     gtk_label_set_text(cantKoban, buffer);
 
-
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
     if (clientSocket == -1) {
         std::cerr << "Error al crear el socket del cliente" << std::endl;
         return 1;
